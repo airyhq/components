d722adc3a900ca717766ae25640f29e03d94fd7d
Bodo Tasche <bitboxer@users.noreply.github.com>
2020-11-18 14:17:09 +0100

    [#4] Create release config for components (#5)
    closes #4

--- components/src/components/alerts/ErrorNotice/index.tsx
+++ components/src/components/alerts/ErrorNotice/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { ReactComponent as ExclamationIcon } from "../../../assets/images/icons/exclamation.svg";
+import React from 'react';
+import {ReactComponent as ExclamationIcon} from '../../../assets/images/icons/exclamation.svg';
 
-import styles from "./style.module.scss";
+import styles from './style.module.scss';
 
 type ErrorNoticeProps = {
   /** Color theme for the error notice */
-  theme: "warning" | "error";
+  theme: 'warning' | 'error';
   /** Error text */
   children: React.ReactNode;
 };
 
-const ErrorNoticeComponent = ({ children, theme }: ErrorNoticeProps) => (
+const ErrorNoticeComponent = ({children, theme}: ErrorNoticeProps) => (
   <div className={`${styles.container} ${styles[theme]}`}>
     <div className={styles.iconWrapper}>
       <ExclamationIcon aria-hidden="true" />
