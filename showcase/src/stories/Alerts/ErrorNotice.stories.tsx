8e8de4c834ad49a3ae327ed44159b4d3de6d101e
Audrey Kadjar <audreykadjar@gmail.com>
2020-12-21 16:20:49 +0100

    fixed scripts for build

--- showcase/src/stories/Alerts/ErrorNotice.stories.tsx
+++ showcase/src/stories/Alerts/ErrorNotice.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import {Story} from '@storybook/react/types-6-0';
 
 import '@airyhq/components/dist/main.css';
